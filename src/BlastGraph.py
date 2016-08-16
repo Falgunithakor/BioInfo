292ecb91b87a71600c2fcb50186e9fa248791067
Falguni Thakor <thako001@cougars.csusm.edu>
2016-08-16 09:40:13 -0700

    Update BlastGraph.py

--- src/BlastGraph.py
+++ src/BlastGraph.py
@@ -1,80 +1,85 @@
-import random
-import time
+import glob
+import os
 
-__author__ = 'FalguniT'
-
-import numpy as np
-import scipy.sparse as sps
 import networkx as nx
-from networkx import nx_pydot
+import time
 from Bio import SearchIO
 import matplotlib.pyplot as plt
 
 
-def main():
-    # read first Blast file
-    Ehux_Chry_FILE_PATH = "../data/blastdata/blastp_Ehux_Chry.o";
-    Ehux_ISO_FILE_PATH = "../data/blastdata/blastp_Ehux_Iso.o";
-    Ehux_Geph_FILE_PATH = "../data/blastdata/blastp_Ehux_Geph.o";
-    Geph_Chry_FILE_PATH = "../data/blastdata/blastp_Geph_Chry.o";
-    Geph_Ehux_FILE_PATH = "../data/blastdata/blastp_Geph_Ehux.o";
-    Geph_Iso_FILE_PATH = "../data/blastdata/blastp_Geph_Iso.o";
-    Iso_Chry_FILE_PATH = "../data/blastdata/blastp_Iso_Chry.o";
-    Iso_Ehux_FILE_PATH = "../data/blastdata/blastp_Iso_Ehux.o";
-    Iso_Geph_FILE_PATH = "../data/blastdata/blastp_Iso_Geph.o";
+class BlastGraph(object):
+    def __init__(self):
+        self.blast_data_path = "../data/blastdata/*.o"
+        self.blast_experiment = ""
+        self.evalue = 1e-10
+        self.blast_output_path = ""
+        self.blast_graph = nx.Graph()
+        self.initialize_variables()
 
-    blast_list = [Ehux_ISO_FILE_PATH,Ehux_Chry_FILE_PATH, Ehux_Geph_FILE_PATH,
-                  Geph_Chry_FILE_PATH, Geph_Ehux_FILE_PATH,Geph_Iso_FILE_PATH,
-                  Iso_Chry_FILE_PATH,Iso_Ehux_FILE_PATH, Iso_Geph_FILE_PATH]
-    g = nx.Graph()
-    data = []
-    evalue_filter = lambda hsp: hsp.evalue < 1e-10
+    def initialize_variables(self):
+        experiment_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+        self.blast_output_path = "../output/blast_connected_graphs_{}".format(experiment_timestamp)
+        os.makedirs(self.blast_output_path)
 
-    for blast_file in blast_list:
-        print(blast_file)
-        result_count = 0
-        qresults = SearchIO.parse(blast_file, 'blast-tab', comments=True)
-        for qresult in qresults:
-            result_count += 1
-            for hit in qresult[:3]:
-                if not g.has_node(qresult.id):
-                    print(qresult.id)
-                    g.add_node(qresult.id)
-                filtered_hit = hit.filter(evalue_filter)
-                if filtered_hit is not None:
-                    if not g.has_node(filtered_hit.id):
-                        g.add_node(filtered_hit.id)
-                    g.add_edge(qresult.id, filtered_hit.id)
+    def generate_blast_graph(self):
+        evalue_filter = lambda hsp: hsp.evalue < self.evalue
+        file_name = "{}/blast_graph.txt".format(self.blast_output_path)
+        for blast_file in glob.glob(self.blast_data_path):
+            print("working on " + blast_file)
+            # Parse the Blast file
+            qresults = SearchIO.parse(blast_file, 'blast-tab', comments=True)
+            for qresult in qresults:
+                #write_line = ""
+                # Go to the Hit section of query
+                for hit in qresult[:3]:
+                    if not self.blast_graph.has_node(qresult.id):
+                        self.blast_graph.add_node(qresult.id)
+                    #write_line += qresult.id + ":"
+                    # Check if Hit has min value 1e-10
+                    filtered_hit = hit.filter(evalue_filter)
+                    if filtered_hit is not None:
+                        if not self.blast_graph.has_node(filtered_hit.id):
+                            self.blast_graph.add_node(filtered_hit.id)
+                        # Add Edge between graph nodes
+                        self.blast_graph.add_edge(qresult.id, filtered_hit.id)
+                        #write_line += filtered_hit.id + ","
+                '''
+                if write_line != "":
+                    with open(file_name, "a") as f_handle:
+                        f_handle.write(write_line + '\n')
+                '''
+        file_name = "{}/blast_graph.gml".format(self.blast_output_path)
+        with open(file_name, "a") as f_handle:
+            nx.write_gml(self.blast_graph, f_handle)
 
-    print("graph has %d nodes with %d edges" \
-          % (nx.number_of_nodes(g), nx.number_of_edges(g)))
-    print(nx.number_connected_components(g), "connected components")
+    def generate_connected_component_graphs(self):
+        # Get Connected Component sub graphs
+        connected_component_sub_graphs = nx.connected_component_subgraphs(self.blast_graph)
+        index = 0
+        plt.figure(1, figsize=(25, 25))
+        for sub_graph in connected_component_sub_graphs:
+            index += 1
+            '''
+            file_name = "{}/graph_{}.txt".format(self.blast_output_path, index)
+            with open(file_name, "a") as f_handle:
+                f_handle.write(str(nx.nodes(sub_graph)) + ':' + str(nx.edges(sub_graph)) + '\n')
+            '''
 
+            file_name = "{}/graph_{}.gml".format(self.blast_output_path,index)
+            with open(file_name, "a") as f_handle:
+                nx.write_gml(sub_graph, f_handle)
 
-    C = nx.connected_component_subgraphs(g)
-    timestamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-    file_name = "../output/blast_connected_graphs_{}.csv".format(timestamp)
-    with open(file_name, "a") as f_handle:
-        for x in C:
-            print("graph has %d nodes with %d edges" % (nx.number_of_nodes(x), nx.number_of_edges(x)))
-            f_handle.write( str(nx.degree(x))+ '\n' )
-            f_handle.write(str(nx.edges(x)) + '\n')
-
-    plt.figure(1, figsize=(25, 25))
-    for x in C:
-        c = [random.random()]*nx.number_of_nodes(x) # random color...
-        nx.draw(x,
-             pos=None,
-             node_size=40,
-             node_color=c,
-             vmin=0.0,
-             vmax=1.0,
-             with_labels=True
-             )
-    plt.show()
-
-
+            '''
+            if len(nx.nodes(sub_graph)) in (5, 25):
+                nx.random_layout(sub_graph)
+                plt.show()
+            '''
 
 
 if __name__ == "__main__":
-    main()
+    objblastgraph = BlastGraph()
+    objblastgraph.generate_blast_graph()
+    print("graph has %d nodes with %d edges" \
+          % (nx.number_of_nodes(objblastgraph.blast_graph), nx.number_of_edges(objblastgraph.blast_graph)))
+    objblastgraph.generate_connected_component_graphs()
+    print(nx.number_connected_components(objblastgraph.blast_graph), "connected components by function")
