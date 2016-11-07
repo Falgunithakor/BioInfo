02e3f64f9fbb13e85e5480a57ada99195614c034
Falguni Thakor <thako001@cougars.csusm.edu>
2016-11-06 18:08:11 -0800

    Update SimilarityNetworks.py

--- src/SimilarityNetworks.py
+++ src/SimilarityNetworks.py
@@ -15,8 +15,7 @@ __author__ = 'FalguniT'
 
 class SimilarityNetworks(object):
     def __init__(self, gene_expression_manager, gene_annotation_manager, evalue=1e-05,
-                 blast_data_path="../data/blastdata/*.o", by_sequence=[], by_desc="", by_go_no="",
-                 by_go_name="", by_enzyme_code="", by_inter_pro_id=""):
+                 blast_data_path="../data/blastdata/*.o", by_sequence=[]):
         self.blast_data_path = blast_data_path
         self.blast_output_path = ""
         self.blast_experiment = ""
@@ -27,11 +26,6 @@ class SimilarityNetworks(object):
         self.gene_annotation_manager = gene_annotation_manager
         # Filtering parameters for selection of sequences
         self.target_sequences = by_sequence
-        self.target_desc = by_desc
-        self.target_GO_no = by_go_no
-        self.target_GO_name = by_go_name
-        self.target_enzyme_code = by_enzyme_code
-        self.target_inter_pro_id = by_inter_pro_id
         self.output_file_identifier = "";
 
     def initialize_variables(self):
@@ -45,58 +39,58 @@ class SimilarityNetworks(object):
         for blast_file in glob.glob(self.blast_data_path):
             # Parse each Blast file
             query_results = SearchIO.parse(blast_file, 'blast-tab', comments=True)
+            filtered_query_results = self.apply_filtering(query_results)
             # Parse each blast record
-            for query_result in query_results:
-                # retrieve Gene expression and gene annotation row for sequence
-                gene_expression_row, gene_annotation_row = self.generate_sequence_gene_annotation_expression_mapping(
-                    query_result.id)
-                is_valid_sequence = self.apply_filtering(query_result, annotation_row=gene_annotation_row)
+            for query_result in filtered_query_results:
+                print query_result.id
+                self.generate_blast_graph(query_result)
 
-                if is_valid_sequence:
-                    print query_result.id, is_valid_sequence, gene_annotation_row
-                    self.generate_blast_graph(query_result, gene_expression_row, gene_annotation_row )
+    def apply_filtering(self, query_results):
+        # Filtering selects specific target sequences from results
+        return (x for x in query_results if x.id in self.target_sequences)
 
-    def apply_filtering(self, query_result, annotation_row):
-        # Filtering applies AND conditions for all filters set
-        is_valid_sequence = True;
-        if len(self.target_sequences) > 0 and query_result.id not in self.target_sequences:
-            is_valid_sequence = False
-        if len(self.target_desc) > 0 and self.target_desc not in annotation_row[1]:
-            is_valid_sequence = False
-        if len(self.target_GO_no) > 0 and self.target_GO_no != int(annotation_row[6]):
-            is_valid_sequence = False
-        if len(self.target_GO_name) > 0 and self.target_GO_name not in annotation_row[7]:
-            is_valid_sequence = False
-        if len(self.target_enzyme_code) > 0 and self.target_enzyme_code not in annotation_row[8]:
-            is_valid_sequence = False
-        if len(self.target_inter_pro_id) > 0 and self.target_inter_pro_id not in annotation_row[9]:
-            is_valid_sequence = False
-        # if any(ext in url_string for ext in extensionsToCheck):
-        return is_valid_sequence
-
-    def generate_blast_graph(self, query_result, gene_expression_row, gene_annotation_row):
+    def generate_blast_graph(self, query_result):
         e_value_filter = lambda hsp: hsp.evalue < self.e_value
         # Add node to graph if not present
         if not self.blast_graph.has_node(query_result.id):
-            self.add_graph_node(query_result.id, gene_expression_row, gene_annotation_row)
+            self.add_graph_node(query_result.id)
         # Go to the Hit section of query
         for hit in query_result[:]:
             # Check if Hit has min e-value
             filtered_hit = hit.filter(e_value_filter)
             if filtered_hit is not None:
                 if not self.blast_graph.has_node(filtered_hit.id):
-                    self.add_graph_node(filtered_hit.id, gene_expression_row, gene_annotation_row)
+                    self.add_graph_node(filtered_hit.id)
                 # Add Edge between graph nodes
-                self.blast_graph.add_edge(query_result.id, filtered_hit.id,
-                                          evalue=str(filtered_hit.hsps[0].evalue),
-                                          identpct=filtered_hit.hsps[0].ident_pct,
-                                          mismatchno=filtered_hit.hsps[0].mismatch_num,
-                                          aln=filtered_hit.hsps[0].aln,
-                                          alnspn=filtered_hit.hsps[0].aln_span,
-                                          gapopenno=filtered_hit.hsps[0].gapopen_num,
-                                          bitscore=filtered_hit.hsps[0].bitscore)
+                if not self.blast_graph.has_edge(query_result.id, filtered_hit.id):
+                    self.blast_graph.add_edge(query_result.id, filtered_hit.id,
+                                              evalue=str(filtered_hit.hsps[0].evalue),
+                                              identpct=filtered_hit.hsps[0].ident_pct,
+                                              mismatchno=filtered_hit.hsps[0].mismatch_num,
+                                              aln=filtered_hit.hsps[0].aln,
+                                              alnspn=filtered_hit.hsps[0].aln_span,
+                                              gapopenno=filtered_hit.hsps[0].gapopen_num,
+                                              bitscore=filtered_hit.hsps[0].bitscore)
+                else:
+                    if filtered_hit.hsps[0].evalue < self.blast_graph[query_result.id][filtered_hit.id]['evalue']:
+                        self.blast_graph[query_result.id][filtered_hit.id]['evalue'] = str(filtered_hit.hsps[0].evalue)
+                        self.blast_graph[query_result.id][filtered_hit.id]['identpct'] = str(
+                            filtered_hit.hsps[0].ident_pct)
+                        self.blast_graph[query_result.id][filtered_hit.id]['mismatchno'] = str(
+                            filtered_hit.hsps[0].mismatch_num)
+                        self.blast_graph[query_result.id][filtered_hit.id]['aln'] = str(filtered_hit.hsps[0].aln)
+                        self.blast_graph[query_result.id][filtered_hit.id]['alnspn'] = str(
+                            filtered_hit.hsps[0].aln_span)
+                        self.blast_graph[query_result.id][filtered_hit.id]['gapopenno'] = str(
+                            filtered_hit.hsps[0].gapopen_num)
+                        self.blast_graph[query_result.id][filtered_hit.id]['bitscore'] = str(
+                            filtered_hit.hsps[0].bitscore)
+                print query_result.id, filtered_hit.hsps[0]
 
-    def add_graph_node(self, sequence_no, gene_expression_row, gene_annotation_row):
+    def add_graph_node(self, sequence_no):
+        # retrieve Gene expression and gene annotation row for sequence
+        gene_expression_row, gene_annotation_row = self.generate_sequence_gene_annotation_expression_mapping(
+            sequence_no)
         self.blast_graph.add_node(sequence_no,
                                   geintercept=gene_expression_row[0][1],
                                   gecacond=gene_expression_row[0][2],
@@ -117,16 +111,27 @@ class SimilarityNetworks(object):
         gene_annotation_row = self.gene_annotation_manager.map_sequence_to_gene_annotation(sequence_no)
         return gene_expression_row, gene_annotation_row
 
+    @staticmethod
+    def get_species_name(name):
+        if name.startswith("KOO"):
+            return "Chry,"
+        elif name.startswith("evm.model.Contig"):
+            return "Geph,"
+        elif name.startswith("evm.model.scaffold"):
+            return "Iso,"
+        else:
+            return "Ehux,"
+
     def write_blast_graph_file(self):
         file_name = "{}/blast_graph.txt".format(self.blast_output_path)
         if len(nx.nodes(self.blast_graph)) > 0:
             with open(file_name, "wb") as f_handle:
                 f_handle.write(
-                    'Source' + ',' + str(
+                    'Source' + ', Species,' + str(
                         [value for value in self.blast_graph.node[nx.nodes(self.blast_graph)[0]].iterkeys()]).strip(
                         '[]') + ',')
                 f_handle.write(
-                    'Target' + ',' + str(
+                    'Target' + ', Species,' + str(
                         [value for value in self.blast_graph.node[nx.nodes(self.blast_graph)[0]].iterkeys()]).strip(
                         '[]') + ',')
                 f_handle.write(
@@ -134,10 +139,12 @@ class SimilarityNetworks(object):
 
                 for u, v, edata in self.blast_graph.edges(data=True):
                     f_handle.write(
-                        str(u) + ',' + str([value for value in self.blast_graph.node[u].itervalues()]).strip(
+                        str(u) + ',' + self.get_species_name(str(u)) + str(
+                            [value for value in self.blast_graph.node[u].itervalues()]).strip(
                             '[]') + ',')
                     f_handle.write(
-                        str(v) + ',' + str([value for value in self.blast_graph.node[v].itervalues()]).strip(
+                        str(v) + ',' + self.get_species_name(str(v)) + str(
+                            [value for value in self.blast_graph.node[v].itervalues()]).strip(
                             '[]') + ',')
                     f_handle.write(
                         str(edata['evalue']) + ',' + str(edata['identpct']) + ',' + str(edata['mismatchno']) + ',' +
@@ -153,17 +160,15 @@ if __name__ == "__main__":
     # Instantiate Gene Expression Manager object
     gene_expression_manager = GeneExpressionManager()
     gene_expression_manager.load_gene_expression_data()
-    # Instantiate Gene Annotaiton Manager object
+    # Instantiate Gene Annotation Manager object
     gene_annotation_manager = GeneAnnotationManager()
     gene_annotation_manager.load_gene_annotation_data()
-    # initialize Filter criteria
-    # By Target sequences
-    # target_sequences = np.genfromtxt("../data/additiona_analysis/Ehux Lipid Metabolism Protein IDs.csv", delimiter='\n',dtype=str)
-    # print target_sequences
-    # By Description
-    target_description = 'transferase'
-    similarity_networks = SimilarityNetworks(gene_expression_manager, gene_annotation_manager,
-                                             by_desc=target_description)
-    similarity_networks.output_file_identifier = "_transferase_for_all_blast_data"
+    # Filter By Target sequences
+    target_sequences = np.genfromtxt("../data/protein_sequence_files/elong.txt", delimiter='\n', dtype=str)
+    print target_sequences
+    print 'Total Target Sequences:', target_sequences.shape
+    similarity_networks = SimilarityNetworks(gene_expression_manager, gene_annotation_manager, evalue=1e-05,
+                                             by_sequence=target_sequences)
+    similarity_networks.output_file_identifier = "_elong_for_all_blast_data"
     similarity_networks.generate_blast_data()
     similarity_networks.write_blast_graph_file()
